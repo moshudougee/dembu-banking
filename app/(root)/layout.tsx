5a656caf1f71fba5bb062ca8581fd721f5588c89
moshudougee <mbuthiadenis@gmail.com>
2024-05-03 22:17:33 +0300

    implement initial file and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+
+
+
+export default function HomeLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        Sidebar
+      {children}
+    </main>
+  );
+}
